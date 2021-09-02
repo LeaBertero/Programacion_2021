9ed277b482b41ff520d32e8aed44a00ed483386c
Leandro <leabertero@hotmail.com>
2021-09-02 01:38:37 -0300

    MI calculadora_por_mi

---
+++ OperacionesEnTexbox/OperacionesEnTexbox/Practica_calculadora.Designer.cs
@@ -0,0 +1,318 @@
+﻿
+namespace OperacionesEnTexbox
+{
+    partial class Practica_calculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnRes_suma = new System.Windows.Forms.Button();
+            this.TxtNumSuma1 = new System.Windows.Forms.TextBox();
+            this.TxtNumSuma2 = new System.Windows.Forms.TextBox();
+            this.LblRes_suma = new System.Windows.Forms.Label();
+            this.LblRes_Resta = new System.Windows.Forms.Label();
+            this.TxtNumResta2 = new System.Windows.Forms.TextBox();
+            this.TxtNumResta1 = new System.Windows.Forms.TextBox();
+            this.BtnRes_resta = new System.Windows.Forms.Button();
+            this.LblRes_Mult = new System.Windows.Forms.Label();
+            this.TxtNumMult2 = new System.Windows.Forms.TextBox();
+            this.TxtNumMult1 = new System.Windows.Forms.TextBox();
+            this.LblRes_Div = new System.Windows.Forms.Label();
+            this.TxtNumDiv2 = new System.Windows.Forms.TextBox();
+            this.TxtNumDiv1 = new System.Windows.Forms.TextBox();
+            this.BtnRes_mult = new System.Windows.Forms.Button();
+            this.BtnRe_div = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnRes_suma
+            // 
+            this.BtnRes_suma.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRes_suma.Location = new System.Drawing.Point(398, 127);
+            this.BtnRes_suma.Name = "BtnRes_suma";
+            this.BtnRes_suma.Size = new System.Drawing.Size(84, 33);
+            this.BtnRes_suma.TabIndex = 0;
+            this.BtnRes_suma.Text = "Calcular";
+            this.BtnRes_suma.UseVisualStyleBackColor = true;
+            this.BtnRes_suma.Click += new System.EventHandler(this.BtnRes_suma_Click);
+            // 
+            // TxtNumSuma1
+            // 
+            this.TxtNumSuma1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNumSuma1.Location = new System.Drawing.Point(119, 127);
+            this.TxtNumSuma1.Name = "TxtNumSuma1";
+            this.TxtNumSuma1.Size = new System.Drawing.Size(100, 29);
+            this.TxtNumSuma1.TabIndex = 1;
+            // 
+            // TxtNumSuma2
+            // 
+            this.TxtNumSuma2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNumSuma2.Location = new System.Drawing.Point(255, 127);
+            this.TxtNumSuma2.Name = "TxtNumSuma2";
+            this.TxtNumSuma2.Size = new System.Drawing.Size(100, 29);
+            this.TxtNumSuma2.TabIndex = 2;
+            // 
+            // LblRes_suma
+            // 
+            this.LblRes_suma.BackColor = System.Drawing.Color.SpringGreen;
+            this.LblRes_suma.Font = new System.Drawing.Font("Kristen ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRes_suma.Location = new System.Drawing.Point(613, 121);
+            this.LblRes_suma.Name = "LblRes_suma";
+            this.LblRes_suma.Size = new System.Drawing.Size(100, 39);
+            this.LblRes_suma.TabIndex = 3;
+            this.LblRes_suma.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LblRes_Resta
+            // 
+            this.LblRes_Resta.BackColor = System.Drawing.Color.SpringGreen;
+            this.LblRes_Resta.Font = new System.Drawing.Font("Kristen ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRes_Resta.Location = new System.Drawing.Point(613, 192);
+            this.LblRes_Resta.Name = "LblRes_Resta";
+            this.LblRes_Resta.Size = new System.Drawing.Size(100, 39);
+            this.LblRes_Resta.TabIndex = 7;
+            this.LblRes_Resta.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TxtNumResta2
+            // 
+            this.TxtNumResta2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNumResta2.Location = new System.Drawing.Point(255, 208);
+            this.TxtNumResta2.Name = "TxtNumResta2";
+            this.TxtNumResta2.Size = new System.Drawing.Size(100, 29);
+            this.TxtNumResta2.TabIndex = 6;
+            // 
+            // TxtNumResta1
+            // 
+            this.TxtNumResta1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNumResta1.Location = new System.Drawing.Point(119, 208);
+            this.TxtNumResta1.Name = "TxtNumResta1";
+            this.TxtNumResta1.Size = new System.Drawing.Size(100, 29);
+            this.TxtNumResta1.TabIndex = 5;
+            // 
+            // BtnRes_resta
+            // 
+            this.BtnRes_resta.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRes_resta.Location = new System.Drawing.Point(398, 198);
+            this.BtnRes_resta.Name = "BtnRes_resta";
+            this.BtnRes_resta.Size = new System.Drawing.Size(84, 33);
+            this.BtnRes_resta.TabIndex = 4;
+            this.BtnRes_resta.Text = "Calcular";
+            this.BtnRes_resta.UseVisualStyleBackColor = true;
+            this.BtnRes_resta.Click += new System.EventHandler(this.BtnRes_resta_Click);
+            // 
+            // LblRes_Mult
+            // 
+            this.LblRes_Mult.BackColor = System.Drawing.Color.SpringGreen;
+            this.LblRes_Mult.Font = new System.Drawing.Font("Kristen ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRes_Mult.Location = new System.Drawing.Point(613, 274);
+            this.LblRes_Mult.Name = "LblRes_Mult";
+            this.LblRes_Mult.Size = new System.Drawing.Size(100, 39);
+            this.LblRes_Mult.TabIndex = 11;
+            this.LblRes_Mult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TxtNumMult2
+            // 
+            this.TxtNumMult2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNumMult2.Location = new System.Drawing.Point(255, 291);
+            this.TxtNumMult2.Name = "TxtNumMult2";
+            this.TxtNumMult2.Size = new System.Drawing.Size(100, 29);
+            this.TxtNumMult2.TabIndex = 10;
+            // 
+            // TxtNumMult1
+            // 
+            this.TxtNumMult1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNumMult1.Location = new System.Drawing.Point(119, 291);
+            this.TxtNumMult1.Name = "TxtNumMult1";
+            this.TxtNumMult1.Size = new System.Drawing.Size(100, 29);
+            this.TxtNumMult1.TabIndex = 9;
+            // 
+            // LblRes_Div
+            // 
+            this.LblRes_Div.BackColor = System.Drawing.Color.SpringGreen;
+            this.LblRes_Div.Font = new System.Drawing.Font("Kristen ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRes_Div.Location = new System.Drawing.Point(613, 355);
+            this.LblRes_Div.Name = "LblRes_Div";
+            this.LblRes_Div.Size = new System.Drawing.Size(100, 39);
+            this.LblRes_Div.TabIndex = 15;
+            this.LblRes_Div.Text = " ";
+            this.LblRes_Div.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TxtNumDiv2
+            // 
+            this.TxtNumDiv2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNumDiv2.Location = new System.Drawing.Point(255, 368);
+            this.TxtNumDiv2.Name = "TxtNumDiv2";
+            this.TxtNumDiv2.Size = new System.Drawing.Size(100, 29);
+            this.TxtNumDiv2.TabIndex = 14;
+            // 
+            // TxtNumDiv1
+            // 
+            this.TxtNumDiv1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtNumDiv1.Location = new System.Drawing.Point(119, 368);
+            this.TxtNumDiv1.Name = "TxtNumDiv1";
+            this.TxtNumDiv1.Size = new System.Drawing.Size(100, 29);
+            this.TxtNumDiv1.TabIndex = 13;
+            // 
+            // BtnRes_mult
+            // 
+            this.BtnRes_mult.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRes_mult.Location = new System.Drawing.Point(398, 278);
+            this.BtnRes_mult.Name = "BtnRes_mult";
+            this.BtnRes_mult.Size = new System.Drawing.Size(84, 33);
+            this.BtnRes_mult.TabIndex = 16;
+            this.BtnRes_mult.Text = "Calcular";
+            this.BtnRes_mult.UseVisualStyleBackColor = true;
+            this.BtnRes_mult.Click += new System.EventHandler(this.BtnRes_mult_Click);
+            // 
+            // BtnRe_div
+            // 
+            this.BtnRe_div.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRe_div.Location = new System.Drawing.Point(398, 361);
+            this.BtnRe_div.Name = "BtnRe_div";
+            this.BtnRe_div.Size = new System.Drawing.Size(84, 33);
+            this.BtnRe_div.TabIndex = 17;
+            this.BtnRe_div.Text = "Calcular";
+            this.BtnRe_div.UseVisualStyleBackColor = true;
+            this.BtnRe_div.Click += new System.EventHandler(this.BtnRe_div_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.SpringGreen;
+            this.label5.Font = new System.Drawing.Font("Kristen ITC", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(255, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(227, 52);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Calculadora";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.HotPink;
+            this.button1.Font = new System.Drawing.Font("Kristen ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(516, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 33);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "/";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.HotPink;
+            this.button2.Font = new System.Drawing.Font("Kristen ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(516, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 33);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "*";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.HotPink;
+            this.button3.Font = new System.Drawing.Font("Kristen ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(516, 198);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(48, 33);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.HotPink;
+            this.button4.Font = new System.Drawing.Font("Kristen ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(516, 127);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(48, 33);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "+";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // Practica_calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(884, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.BtnRe_div);
+            this.Controls.Add(this.BtnRes_mult);
+            this.Controls.Add(this.LblRes_Div);
+            this.Controls.Add(this.TxtNumDiv2);
+            this.Controls.Add(this.TxtNumDiv1);
+            this.Controls.Add(this.LblRes_Mult);
+            this.Controls.Add(this.TxtNumMult2);
+            this.Controls.Add(this.TxtNumMult1);
+            this.Controls.Add(this.LblRes_Resta);
+            this.Controls.Add(this.TxtNumResta2);
+            this.Controls.Add(this.TxtNumResta1);
+            this.Controls.Add(this.BtnRes_resta);
+            this.Controls.Add(this.LblRes_suma);
+            this.Controls.Add(this.TxtNumSuma2);
+            this.Controls.Add(this.TxtNumSuma1);
+            this.Controls.Add(this.BtnRes_suma);
+            this.Name = "Practica_calculadora";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnRes_suma;
+        private System.Windows.Forms.TextBox TxtNumSuma1;
+        private System.Windows.Forms.TextBox TxtNumSuma2;
+        private System.Windows.Forms.Label LblRes_suma;
+        private System.Windows.Forms.Label LblRes_Resta;
+        private System.Windows.Forms.TextBox TxtNumResta2;
+        private System.Windows.Forms.TextBox TxtNumResta1;
+        private System.Windows.Forms.Button BtnRes_resta;
+        private System.Windows.Forms.Label LblRes_Mult;
+        private System.Windows.Forms.TextBox TxtNumMult2;
+        private System.Windows.Forms.TextBox TxtNumMult1;
+        private System.Windows.Forms.Label LblRes_Div;
+        private System.Windows.Forms.TextBox TxtNumDiv2;
+        private System.Windows.Forms.TextBox TxtNumDiv1;
+        private System.Windows.Forms.Button BtnRes_mult;
+        private System.Windows.Forms.Button BtnRe_div;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ OperacionesEnTexbox/OperacionesEnTexbox/Practica_calculadora.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OperacionesEnTexbox
+{
+    public partial class Practica_calculadora : Form
+    {
+        public Practica_calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRes_suma_Click(object sender, EventArgs e)
+        {
+            int numero1;
+            int numero2;
+
+            int resultado;
+
+            numero1 = System.Convert.ToInt32(TxtNumSuma1.Text);
+            numero2 = System.Convert.ToInt32(TxtNumSuma2.Text);
+
+            resultado = numero1 + numero2;
+
+            LblRes_suma.Text = System.Convert.ToString(resultado);
+        }
+
+        private void BtnRes_resta_Click(object sender, EventArgs e)
+        {
+            int numero1;
+            int numero2;
+
+            int resultado;
+
+            numero1 = System.Convert.ToInt32(TxtNumResta1.Text);
+            numero2 = System.Convert.ToInt32(TxtNumResta2.Text);
+
+            resultado = numero1 - numero2;
+
+            LblRes_Resta.Text = System.Convert.ToString(resultado);
+        }
+
+        private void BtnRes_mult_Click(object sender, EventArgs e)
+        {
+            int numero1;
+            int numero2;
+
+            int resultado;
+
+            numero1 = System.Convert.ToInt32(TxtNumMult1.Text);
+            numero2 = System.Convert.ToInt32(TxtNumMult2.Text);
+
+            resultado = numero1 * numero2;
+
+            LblRes_Mult.Text = System.Convert.ToString(resultado);
+        }
+
+        private void BtnRe_div_Click(object sender, EventArgs e)
+        {
+            int numero1;
+            int numero2;
+
+            int resultado;
+
+            numero1 = System.Convert.ToInt32(TxtNumDiv1.Text);
+            numero2 = System.Convert.ToInt32(TxtNumDiv2.Text);
+
+            resultado = numero1 / numero2;
+
+            LblRes_Div.Text = System.Convert.ToString(resultado);
+        }
+    }
+}

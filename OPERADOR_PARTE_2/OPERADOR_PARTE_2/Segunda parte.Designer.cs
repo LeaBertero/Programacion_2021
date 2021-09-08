4f84da33c555dbe623fc3da812fd45358b47d1fe
Leandro <leabertero@hotmail.com>
2021-09-08 20:41:03 -0300

    Calculadora con swich (Segun)

---
+++ OPERADOR_PARTE_2/OPERADOR_PARTE_2/Segunda parte.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace OPERADOR_PARTE_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnRes = new System.Windows.Forms.Button();
+            this.TxtOper2 = new System.Windows.Forms.TextBox();
+            this.TxtOperador = new System.Windows.Forms.TextBox();
+            this.TxtOper1 = new System.Windows.Forms.TextBox();
+            this.LblRes = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnRes
+            // 
+            this.BtnRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRes.Location = new System.Drawing.Point(419, 126);
+            this.BtnRes.Name = "BtnRes";
+            this.BtnRes.Size = new System.Drawing.Size(104, 28);
+            this.BtnRes.TabIndex = 0;
+            this.BtnRes.Text = "Resultado";
+            this.BtnRes.UseVisualStyleBackColor = true;
+            this.BtnRes.Click += new System.EventHandler(this.BtnRes_Click);
+            // 
+            // TxtOper2
+            // 
+            this.TxtOper2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtOper2.Location = new System.Drawing.Point(289, 126);
+            this.TxtOper2.Name = "TxtOper2";
+            this.TxtOper2.Size = new System.Drawing.Size(100, 26);
+            this.TxtOper2.TabIndex = 1;
+            // 
+            // TxtOperador
+            // 
+            this.TxtOperador.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtOperador.Location = new System.Drawing.Point(160, 126);
+            this.TxtOperador.Name = "TxtOperador";
+            this.TxtOperador.Size = new System.Drawing.Size(100, 26);
+            this.TxtOperador.TabIndex = 2;
+            // 
+            // TxtOper1
+            // 
+            this.TxtOper1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtOper1.Location = new System.Drawing.Point(37, 126);
+            this.TxtOper1.Name = "TxtOper1";
+            this.TxtOper1.Size = new System.Drawing.Size(100, 26);
+            this.TxtOper1.TabIndex = 3;
+            // 
+            // LblRes
+            // 
+            this.LblRes.BackColor = System.Drawing.Color.Orange;
+            this.LblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRes.Location = new System.Drawing.Point(553, 126);
+            this.LblRes.Name = "LblRes";
+            this.LblRes.Size = new System.Drawing.Size(111, 28);
+            this.LblRes.TabIndex = 4;
+            this.LblRes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Operador 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(286, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Operador 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(157, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Operación";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 244);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LblRes);
+            this.Controls.Add(this.TxtOper1);
+            this.Controls.Add(this.TxtOperador);
+            this.Controls.Add(this.TxtOper2);
+            this.Controls.Add(this.BtnRes);
+            this.Name = "Form1";
+            this.Text = "Operadores - Parte 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnRes;
+        private System.Windows.Forms.TextBox TxtOper2;
+        private System.Windows.Forms.TextBox TxtOperador;
+        private System.Windows.Forms.TextBox TxtOper1;
+        private System.Windows.Forms.Label LblRes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ OPERADOR_PARTE_2/OPERADOR_PARTE_2/Segunda parte.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OPERADOR_PARTE_2
+{
+    public partial class Form1 : Form
+    {
+         
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        
+
+        private void BtnRes_Click(object sender, EventArgs e)
+        {
+            int numero1;
+            int numero2;
+            int resultado;
+
+            switch (TxtOperador.Text)
+            {
+                case "+":
+                     numero1 = System.Convert.ToInt32(TxtOper1.Text);
+                     numero2 = System.Convert.ToInt32(TxtOper2.Text);
+                     
+                    resultado = numero1 + numero2;
+
+                     LblRes.Text = resultado.ToString();
+
+                    break;
+
+                    case "-":
+
+                    numero1 = System.Convert.ToInt32(TxtOper1.Text);
+                    numero2 = System.Convert.ToInt32(TxtOper2.Text);
+                    
+                    resultado = numero1 - numero2;
+
+                    LblRes.Text = resultado.ToString();
+
+                    break;
+
+                case "*":
+                    numero1 = System.Convert.ToInt32(TxtOper1.Text);
+                    numero2 = System.Convert.ToInt32(TxtOper2.Text);
+
+                    resultado = numero1 * numero2;
+
+                    LblRes.Text = resultado.ToString();
+
+                    break;
+
+                case "/":
+                    numero1 = System.Convert.ToInt32(TxtOper1.Text);
+                    numero2 = System.Convert.ToInt32(TxtOper2.Text);
+
+                    resultado = numero1 / numero2;
+
+                    LblRes.Text = resultado.ToString();
+
+                    break;
+
+                default:
+
+                    LblRes.Text = "Error";
+                    break;
+            }
+        }
+    }
+}

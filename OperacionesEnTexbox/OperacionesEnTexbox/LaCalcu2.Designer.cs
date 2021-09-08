63964822664d56b770514052d0479bfea4ff8104
Leandro <leabertero@hotmail.com>
2021-09-08 10:04:15 -0300

    cambios en diseño

--- OperacionesEnTexbox/OperacionesEnTexbox/LaCalcu2.Designer.cs
+++ OperacionesEnTexbox/OperacionesEnTexbox/LaCalcu2.Designer.cs
@@ -47,7 +47,7 @@ namespace OperacionesEnTexbox
             // 
             this.BtnCalc.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.BtnCalc.Font = new System.Drawing.Font("Lucida Bright", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnCalc.Location = new System.Drawing.Point(603, 185);
+            this.BtnCalc.Location = new System.Drawing.Point(546, 180);
             this.BtnCalc.Name = "BtnCalc";
             this.BtnCalc.Size = new System.Drawing.Size(111, 57);
             this.BtnCalc.TabIndex = 0;
@@ -58,7 +58,7 @@ namespace OperacionesEnTexbox
             // TxtNum1
             // 
             this.TxtNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtNum1.Location = new System.Drawing.Point(84, 204);
+            this.TxtNum1.Location = new System.Drawing.Point(23, 216);
             this.TxtNum1.Name = "TxtNum1";
             this.TxtNum1.Size = new System.Drawing.Size(139, 26);
             this.TxtNum1.TabIndex = 1;
@@ -66,7 +66,7 @@ namespace OperacionesEnTexbox
             // TxtOperacion
             // 
             this.TxtOperacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtOperacion.Location = new System.Drawing.Point(261, 204);
+            this.TxtOperacion.Location = new System.Drawing.Point(200, 216);
             this.TxtOperacion.Name = "TxtOperacion";
             this.TxtOperacion.Size = new System.Drawing.Size(139, 26);
             this.TxtOperacion.TabIndex = 2;
@@ -74,7 +74,7 @@ namespace OperacionesEnTexbox
             // TxtNum2
             // 
             this.TxtNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtNum2.Location = new System.Drawing.Point(433, 204);
+            this.TxtNum2.Location = new System.Drawing.Point(372, 216);
             this.TxtNum2.Name = "TxtNum2";
             this.TxtNum2.Size = new System.Drawing.Size(139, 26);
             this.TxtNum2.TabIndex = 3;
@@ -87,7 +87,7 @@ namespace OperacionesEnTexbox
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.label1.Font = new System.Drawing.Font("MV Boli", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(337, 9);
+            this.label1.Location = new System.Drawing.Point(29, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(310, 109);
             this.label1.TabIndex = 4;
@@ -99,7 +99,7 @@ namespace OperacionesEnTexbox
             // 
             this.LblRes.BackColor = System.Drawing.Color.Orange;
             this.LblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LblRes.Location = new System.Drawing.Point(831, 185);
+            this.LblRes.Location = new System.Drawing.Point(804, 180);
             this.LblRes.Name = "LblRes";
             this.LblRes.Size = new System.Drawing.Size(125, 57);
             this.LblRes.TabIndex = 5;
@@ -110,7 +110,7 @@ namespace OperacionesEnTexbox
             this.button1.BackColor = System.Drawing.SystemColors.Desktop;
             this.button1.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(748, 188);
+            this.button1.Location = new System.Drawing.Point(704, 183);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(51, 49);
             this.button1.TabIndex = 6;
@@ -125,7 +125,7 @@ namespace OperacionesEnTexbox
             this.label3.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.label3.Font = new System.Drawing.Font("MV Boli", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label3.Location = new System.Drawing.Point(84, 148);
+            this.label3.Location = new System.Drawing.Point(23, 160);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(139, 32);
             this.label3.TabIndex = 7;
@@ -140,7 +140,7 @@ namespace OperacionesEnTexbox
             this.label4.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.label4.Font = new System.Drawing.Font("MV Boli", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label4.Location = new System.Drawing.Point(261, 148);
+            this.label4.Location = new System.Drawing.Point(200, 160);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(139, 32);
             this.label4.TabIndex = 8;
@@ -155,7 +155,7 @@ namespace OperacionesEnTexbox
             this.label5.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.label5.Font = new System.Drawing.Font("MV Boli", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label5.Location = new System.Drawing.Point(433, 148);
+            this.label5.Location = new System.Drawing.Point(372, 160);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(139, 32);
             this.label5.TabIndex = 9;
@@ -168,9 +168,9 @@ namespace OperacionesEnTexbox
             this.BtnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
             this.BtnSalir.Font = new System.Drawing.Font("Kristen ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnSalir.ForeColor = System.Drawing.Color.OrangeRed;
-            this.BtnSalir.Location = new System.Drawing.Point(884, 12);
+            this.BtnSalir.Location = new System.Drawing.Point(859, 12);
             this.BtnSalir.Name = "BtnSalir";
-            this.BtnSalir.Size = new System.Drawing.Size(124, 49);
+            this.BtnSalir.Size = new System.Drawing.Size(125, 46);
             this.BtnSalir.TabIndex = 29;
             this.BtnSalir.Text = "( Salir ) ->";
             this.BtnSalir.UseVisualStyleBackColor = false;
@@ -181,7 +181,7 @@ namespace OperacionesEnTexbox
             this.LblMensajeError.BackColor = System.Drawing.Color.Black;
             this.LblMensajeError.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LblMensajeError.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.LblMensajeError.Location = new System.Drawing.Point(831, 96);
+            this.LblMensajeError.Location = new System.Drawing.Point(804, 96);
             this.LblMensajeError.Name = "LblMensajeError";
             this.LblMensajeError.Size = new System.Drawing.Size(125, 57);
             this.LblMensajeError.TabIndex = 30;

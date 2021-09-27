c1b803c751c3ddc7ceb4849711a86af0f7607554
Leandro Bertero <leabertero@hotmail.com>
2021-09-27 00:48:20 -0300

    prueba movimiento

---
+++ Repeticion_Aleatoria/Repeticion_Aleatoria/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Repeticion_Aleatoria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lbl_aleatorio = new System.Windows.Forms.Label();
+            this.Btn_comenzar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Lbl_aleatorio
+            // 
+            this.Lbl_aleatorio.BackColor = System.Drawing.Color.Red;
+            this.Lbl_aleatorio.Location = new System.Drawing.Point(12, 9);
+            this.Lbl_aleatorio.Name = "Lbl_aleatorio";
+            this.Lbl_aleatorio.Size = new System.Drawing.Size(117, 73);
+            this.Lbl_aleatorio.TabIndex = 0;
+            // 
+            // Btn_comenzar
+            // 
+            this.Btn_comenzar.Location = new System.Drawing.Point(528, 97);
+            this.Btn_comenzar.Name = "Btn_comenzar";
+            this.Btn_comenzar.Size = new System.Drawing.Size(95, 54);
+            this.Btn_comenzar.TabIndex = 1;
+            this.Btn_comenzar.Text = "Comenzar";
+            this.Btn_comenzar.UseVisualStyleBackColor = true;
+            this.Btn_comenzar.Click += new System.EventHandler(this.Btn_comenzar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Btn_comenzar);
+            this.Controls.Add(this.Lbl_aleatorio);
+            this.Name = "Form1";
+            this.Text = "Repeticion aleatoria";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl_aleatorio;
+        private System.Windows.Forms.Button Btn_comenzar;
+    }
+}
+
---
+++ Repeticion_Aleatoria/Repeticion_Aleatoria/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Repeticion_Aleatoria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_comenzar_Click(object sender, EventArgs e)
+        {
+            int sentidoV = 1;
+            int sentidoH = 1;
+            int PasoV = 1;
+            int pasoH = 1;
+
+            for (int i = 0; i < 100; i++)
+            {
+                Lbl_aleatorio.Left = Lbl_aleatorio.Left + (pasoH * sentidoH);
+                Lbl_aleatorio.Top = Lbl_aleatorio.Top + (PasoV * sentidoV);
+                this.Refresh();
+            }
+        }
+    }
+}

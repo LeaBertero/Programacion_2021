0abf688e441947280b596a8c0b85db5e398233ab
Leandro Bertero <leabertero@hotmail.com>
2021-09-28 21:45:42 -0300

    cambios en controles

---
+++ Ejemplo_carga_de_personas/Ejemplo_carga_de_personas/Inscripión_materias.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo_carga_de_personas
+{
+    public partial class Inscripión_materias : Form
+    {
+        public Inscripión_materias()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}

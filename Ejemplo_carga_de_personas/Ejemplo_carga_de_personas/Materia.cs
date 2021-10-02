8fc997738dcd6d92d290ff4571c7591c3f2da892
Leandro <leabertero@hotmail.com>
2021-10-02 16:28:35 -0300

    Se cambio el nombre de name space por (Primer Clase)

--- Ejemplo_carga_de_personas/Ejemplo_carga_de_personas/Materia.cs
+++ Ejemplo_carga_de_personas/Ejemplo_carga_de_personas/Materia.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ejemplo_carga_de_personas
+namespace Primer_CLase
 {
     public class Materia
     {
--- Ejemplo_carga_de_personas/Ejemplo_carga_de_personas/Persona.cs
+++ Ejemplo_carga_de_personas/Ejemplo_carga_de_personas/Persona.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ejemplo_carga_de_personas
+namespace Primer_CLase
 {
     public class Persona
     {
@@ -12,13 +12,22 @@ namespace Ejemplo_carga_de_personas
 
         public string Apellido { get; set; }
 
-        public Materia MateriaCursando { get; set; }
+        public Materia MateriaCursando { get; set; } = new Materia();
 
-        public string NombreCompleto() 
+        public string NombreCompleto()
         {
             return Nombre + " " + Apellido;
         }
+
+        public void Inscribir(Materia materia)
+        {
+            MateriaCursando = materia;
+        }
     }
 
-   
+    
+        
 }
+
+   
+

4be25c00f4509235f5fa653a57c123eafcd571bc
Leandro <leabertero@hotmail.com>
2021-10-05 00:43:16 -0300

    Dias de la semana, practica de (Arreglos)

---
+++ PrimerArreglo/PrimerArreglo/Primer_Arreglo.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace PrimerArreglo
+{
+    partial class Primer_Arreglo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.LblDia0 = new System.Windows.Forms.Label();
+            this.LblDia1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.LblDia2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.LblDia3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.LblDia4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.LblDia5 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.LblDia6 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.BtnSalida = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(58, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mostrar día 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LblDia0
+            // 
+            this.LblDia0.Cursor = System.Windows.Forms.Cursors.Default;
+            this.LblDia0.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDia0.Location = new System.Drawing.Point(65, 164);
+            this.LblDia0.Name = "LblDia0";
+            this.LblDia0.Size = new System.Drawing.Size(100, 23);
+            this.LblDia0.TabIndex = 1;
+            this.LblDia0.Visible = false;
+            // 
+            // LblDia1
+            // 
+            this.LblDia1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.LblDia1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDia1.Location = new System.Drawing.Point(204, 164);
+            this.LblDia1.Name = "LblDia1";
+            this.LblDia1.Size = new System.Drawing.Size(100, 23);
+            this.LblDia1.TabIndex = 3;
+            this.LblDia1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(197, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Mostrar día 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // LblDia2
+            // 
+            this.LblDia2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.LblDia2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDia2.Location = new System.Drawing.Point(335, 164);
+            this.LblDia2.Name = "LblDia2";
+            this.LblDia2.Size = new System.Drawing.Size(100, 23);
+            this.LblDia2.TabIndex = 5;
+            this.LblDia2.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(328, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Mostrar día 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // LblDia3
+            // 
+            this.LblDia3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.LblDia3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDia3.Location = new System.Drawing.Point(466, 164);
+            this.LblDia3.Name = "LblDia3";
+            this.LblDia3.Size = new System.Drawing.Size(100, 23);
+            this.LblDia3.TabIndex = 7;
+            this.LblDia3.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(459, 108);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Mostrar día 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // LblDia4
+            // 
+            this.LblDia4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.LblDia4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDia4.Location = new System.Drawing.Point(596, 164);
+            this.LblDia4.Name = "LblDia4";
+            this.LblDia4.Size = new System.Drawing.Size(100, 23);
+            this.LblDia4.TabIndex = 9;
+            this.LblDia4.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(589, 108);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Mostrar día 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // LblDia5
+            // 
+            this.LblDia5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.LblDia5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDia5.Location = new System.Drawing.Point(726, 164);
+            this.LblDia5.Name = "LblDia5";
+            this.LblDia5.Size = new System.Drawing.Size(100, 23);
+            this.LblDia5.TabIndex = 11;
+            this.LblDia5.Visible = false;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(719, 108);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(107, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Mostrar día 6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // LblDia6
+            // 
+            this.LblDia6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.LblDia6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDia6.Location = new System.Drawing.Point(859, 164);
+            this.LblDia6.Name = "LblDia6";
+            this.LblDia6.Size = new System.Drawing.Size(100, 23);
+            this.LblDia6.TabIndex = 13;
+            this.LblDia6.Visible = false;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(852, 108);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(107, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Mostrar día 7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // BtnSalida
+            // 
+            this.BtnSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSalida.Location = new System.Drawing.Point(926, 453);
+            this.BtnSalida.Name = "BtnSalida";
+            this.BtnSalida.Size = new System.Drawing.Size(78, 23);
+            this.BtnSalida.TabIndex = 14;
+            this.BtnSalida.Text = "Salida";
+            this.BtnSalida.UseVisualStyleBackColor = true;
+            this.BtnSalida.Click += new System.EventHandler(this.BtnSalida_Click);
+            // 
+            // Primer_Arreglo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 488);
+            this.Controls.Add(this.BtnSalida);
+            this.Controls.Add(this.LblDia6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.LblDia5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.LblDia4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.LblDia3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.LblDia2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.LblDia1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.LblDia0);
+            this.Controls.Add(this.button1);
+            this.Name = "Primer_Arreglo";
+            this.Text = "Primer Arreglo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label LblDia0;
+        private System.Windows.Forms.Label LblDia1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label LblDia2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label LblDia3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label LblDia4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label LblDia5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label LblDia6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button BtnSalida;
+    }
+}
+
---
+++ PrimerArreglo/PrimerArreglo/Primer_Arreglo.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimerArreglo
+{
+    public partial class Primer_Arreglo : Form
+    {
+        public Primer_Arreglo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] Dias = new string[7];
+
+            Dias[0] = "Lunes";
+            Dias[1] = "Martes";
+            Dias[2] = "Miércoles";
+            Dias[3] = "Jueves";
+            Dias[4] = "Viernes";
+            Dias[5] = "Sábado";
+            Dias[6] = "Domingo";
+
+            LblDia0.Text = Dias [0];
+            LblDia0.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] Dias = new string[7];
+
+            Dias[0] = "Lunes";
+            Dias[1] = "Martes";
+            Dias[2] = "Miércoles";
+            Dias[3] = "Jueves";
+            Dias[4] = "Viernes";
+            Dias[5] = "Sábado";
+            Dias[6] = "Domingo";
+
+            LblDia1.Text = Dias[1];
+            LblDia1.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] Dias = new string[7];
+
+            Dias[0] = "Lunes";
+            Dias[1] = "Martes";
+            Dias[2] = "Miércoles";
+            Dias[3] = "Jueves";
+            Dias[4] = "Viernes";
+            Dias[5] = "Sábado";
+            Dias[6] = "Domingo";
+
+            LblDia2.Text = Dias[2];
+            LblDia2.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string[] Dias = new string[7];
+
+            Dias[0] = "Lunes";
+            Dias[1] = "Martes";
+            Dias[2] = "Miércoles";
+            Dias[3] = "Jueves";
+            Dias[4] = "Viernes";
+            Dias[5] = "Sábado";
+            Dias[6] = "Domingo";
+
+            LblDia3.Text = Dias[3];
+            LblDia3.Visible = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string[] Dias = new string[7];
+
+            Dias[0] = "Lunes";
+            Dias[1] = "Martes";
+            Dias[2] = "Miércoles";
+            Dias[3] = "Jueves";
+            Dias[4] = "Viernes";
+            Dias[5] = "Sábado";
+            Dias[6] = "Domingo";
+
+            LblDia4.Text = Dias[4];
+            LblDia4.Visible = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string[] Dias = new string[7];
+
+            Dias[0] = "Lunes";
+            Dias[1] = "Martes";
+            Dias[2] = "Miércoles";
+            Dias[3] = "Jueves";
+            Dias[4] = "Viernes";
+            Dias[5] = "Sábado";
+            Dias[6] = "Domingo";
+
+            LblDia5.Text = Dias[5];
+            LblDia5.Visible = true;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string[] Dias = new string[7];
+
+            Dias[0] = "Lunes";
+            Dias[1] = "Martes";
+            Dias[2] = "Miércoles";
+            Dias[3] = "Jueves";
+            Dias[4] = "Viernes";
+            Dias[5] = "Sábado";
+            Dias[6] = "Domingo";
+
+            LblDia6.Text = Dias[6];
+            LblDia6.Visible = true;
+        }
+
+        private void BtnSalida_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}

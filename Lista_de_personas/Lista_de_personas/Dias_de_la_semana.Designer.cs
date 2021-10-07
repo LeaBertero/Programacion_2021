e20f77a0a22044849d8f8b7df9f2ad551e6fecb4
Leandro <leabertero@hotmail.com>
2021-10-07 00:41:12 -0300

    nuevo diseño de carga de nombres

--- Lista_de_personas/Lista_de_personas/Dias_de_la_semana.Designer.cs
+++ Lista_de_personas/Lista_de_personas/Dias_de_la_semana.Designer.cs
@@ -32,6 +32,10 @@ namespace Lista_de_personas
             this.BtnSemana = new System.Windows.Forms.Button();
             this.LblSemana = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // BtnSemana
@@ -69,19 +73,66 @@ namespace Lista_de_personas
             this.label1.Text = "- Días de la semana -";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(913, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Cargar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Teal;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(773, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 53);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "* Carga de personas *";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(778, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.LightCoral;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(807, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 202);
+            this.label3.TabIndex = 6;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Visible = false;
+            // 
             // Dias_de_la_semana
             // 
             this.AcceptButton = this.BtnSemana;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DeepPink;
-            this.ClientSize = new System.Drawing.Size(436, 449);
+            this.ClientSize = new System.Drawing.Size(1209, 449);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.LblSemana);
             this.Controls.Add(this.BtnSemana);
             this.Name = "Dias_de_la_semana";
             this.Text = "Dias de la semana";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -90,6 +141,10 @@ namespace Lista_de_personas
         private System.Windows.Forms.Button BtnSemana;
         private System.Windows.Forms.Label LblSemana;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
 

a58c5516c88b2ea9eb6b8e17fea5b2ab5b213871
Leandro <leabertero@hotmail.com>
2021-10-12 01:12:35 -0300

    Primer version - Prueba

---
+++ Ejemplo_lista/Ejemplo_lista/Ejemplo_lista_personas.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace Ejemplo_lista
+{
+    partial class Ejemplo_lista_personas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCargar = new System.Windows.Forms.Button();
+            this.TxtCarga = new System.Windows.Forms.TextBox();
+            this.LblLista = new System.Windows.Forms.Label();
+            this.BtnMostrar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnCargar
+            // 
+            this.BtnCargar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCargar.Location = new System.Drawing.Point(184, 22);
+            this.BtnCargar.Name = "BtnCargar";
+            this.BtnCargar.Size = new System.Drawing.Size(115, 35);
+            this.BtnCargar.TabIndex = 0;
+            this.BtnCargar.Text = "Cargar";
+            this.BtnCargar.UseVisualStyleBackColor = true;
+            this.BtnCargar.Click += new System.EventHandler(this.BtnCargar_Click);
+            // 
+            // TxtCarga
+            // 
+            this.TxtCarga.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtCarga.Location = new System.Drawing.Point(26, 26);
+            this.TxtCarga.Name = "TxtCarga";
+            this.TxtCarga.Size = new System.Drawing.Size(126, 26);
+            this.TxtCarga.TabIndex = 1;
+            // 
+            // LblLista
+            // 
+            this.LblLista.AutoSize = true;
+            this.LblLista.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblLista.Location = new System.Drawing.Point(22, 85);
+            this.LblLista.Name = "LblLista";
+            this.LblLista.Size = new System.Drawing.Size(188, 33);
+            this.LblLista.TabIndex = 3;
+            this.LblLista.Text = "Lista de personas:";
+            // 
+            // BtnMostrar
+            // 
+            this.BtnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnMostrar.Location = new System.Drawing.Point(337, 22);
+            this.BtnMostrar.Name = "BtnMostrar";
+            this.BtnMostrar.Size = new System.Drawing.Size(115, 35);
+            this.BtnMostrar.TabIndex = 4;
+            this.BtnMostrar.Text = "Mostrar";
+            this.BtnMostrar.UseVisualStyleBackColor = true;
+            this.BtnMostrar.Click += new System.EventHandler(this.BtnMostrar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(362, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Cerrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Ejemplo_lista_personas
+            // 
+            this.AcceptButton = this.BtnCargar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 302);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnMostrar);
+            this.Controls.Add(this.LblLista);
+            this.Controls.Add(this.TxtCarga);
+            this.Controls.Add(this.BtnCargar);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Ejemplo_lista_personas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Programa - Ejemplo carga de una lista de personas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCargar;
+        private System.Windows.Forms.TextBox TxtCarga;
+        private System.Windows.Forms.Label LblLista;
+        private System.Windows.Forms.Button BtnMostrar;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Ejemplo_lista/Ejemplo_lista/Ejemplo_lista_personas.cs
@@ -0,0 +1,46 @@
+﻿using Ejemplo_lista.Clases_Para_Formulario;
+using System;
+using System.Windows.Forms;
+
+namespace Ejemplo_lista
+{
+    public partial class Ejemplo_lista_personas : Form
+    {
+        //Esta lista, es un arreglo de personas
+        public Persona[] Personas { get; set; } = new Persona [3];
+
+        public int Posicion = -1;
+        public Ejemplo_lista_personas()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCargar_Click(object sender, EventArgs e)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = TxtCarga.Text;
+            Posicion = Posicion + 1;
+            Personas[Posicion] = persona;
+        }
+
+        private void BtnMostrar_Click(object sender, EventArgs e)
+        {
+            LblLista.Text = "Lista de personas: \r\n";
+            foreach (Persona item in Personas)
+            {
+                LblLista.Text = LblLista.Text + item.Nombre + "\r\n";
+            }
+                
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
+        
+
+
+
